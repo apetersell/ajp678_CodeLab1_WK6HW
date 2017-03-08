18a1e69f19e5895433849af67700573901bfee8c
Unknown <andrewpetersell@gmail.com>
2017-03-07 21:30:14 -0500

    Can Read City Name/Region/Wind Speed

---
+++ Week 6 HW/Assets/Scripts/GetSpreadSheetInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using System.IO;
+using System.Net;
+using UnityEngine.UI;
+
+
+public class GetSpreadSheetInfo : MonoBehaviour {
+
+	public string city;
+	public string state;
+	WebClient client; 
+	public Text cityText;
+	public Text regionText;
+	public Text windText; 
+	Text tCity;
+	Text tRegion;
+	Text tWind;
+
+	// Use this for initialization
+	void Start () {
+		client = new WebClient ();
+		tCity = cityText.GetComponent<Text> ();
+		tRegion = regionText.GetComponent<Text> ();
+		tWind = windText.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		string cityInfo = client.DownloadString ("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%2C%20" + state + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+		JSONNode info = JSON.Parse (cityInfo);
+
+
+		string cityName = info["query"]["results"]["channel"]["location"]["city"];
+		string regionName = info ["query"] ["results"] ["channel"] ["location"] ["region"];
+		string windSpeed = info ["query"] ["results"] ["channel"] ["wind"] ["speed"];
+		Debug.Log ("Current Wind Speed: " + windSpeed);
+
+		tCity.text = cityName;
+		tRegion.text = regionName;
+		tWind.text = windSpeed;
+	}
+
+
+}
--- Week 6 HW/Assets/Scripts/Week6Library.cs
+++ Week 6 HW/Assets/Scripts/Week6Library.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
+using System.IO;
 
-public class Week6Library : MonoBehaviour {
+public class Week6Library : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
+
+	/// <summary>
+	/// Write a string to a file
+	/// </summary>
+	/// <param name="path">Path to file to write</param> 
+	/// <param name="fileName">Name of file to write</param>
+	/// <param name="content">String to write to the file.</param> 
+	public static void WriteStringtoFile (string path, string fileName, string content)  
+	{
+		StreamWriter sw = new StreamWriter (path + "/" + fileName); 
+		sw.Write (content);
+		sw.Close ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	/// <summary>
+	/// Read a file to end.
+	/// </summary>
+	/// <param name="path">Path to file to read</param> 
+	/// <param name="fileName">Name of file to write</param>
+	public static string ReadStringToEnd (string path, string fileName) 
+	{
+		StreamReader sr = new StreamReader(path + "/" + fileName);
+		string result = sr.ReadToEnd();
+		sr.Close();
+		return result;
+	}
+
+	/// <summary>
+	/// Write a JSON to a file.
+	/// </summary>
+	/// <param name="path">Path to file to write</param> 
+	/// <param name="fileName">Name of JSON file to write</param>
+	/// <param name="json">JSON to write to file.</param> 
+	public static void WriteJSONtoFile (string path, string fileName, JSONClass json)
+	{
+		WriteStringtoFile (path, fileName, json.ToString ()); 
+	}
+
+	/// <summary>
+	/// Read a JSON file.
+	/// </summary>
+	/// <param name="path">Path to file to write</param> 
+	/// <param name="fileName">Name of JSON file to read</param>
+	public static JSONNode ReadJSONtoFile (string path, string fileName)
+	{
+		JSONNode result = JSON.Parse(ReadStringToEnd(path, fileName));
+		return result;
 	}
 }
